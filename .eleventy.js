b70f7e28079dd7cc9937cf597b9c7ad3a5ecaa6f
Julien Labatut <skoognuts@outlook.fr>
2022-02-22 23:44:24 +0100

    11TY: Initiate 11Ty Config File.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 /* Configuration File */
 
 module.exports = function(eleventyConfig) {
+  
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
   return {
     dir: {
       input: "src",
