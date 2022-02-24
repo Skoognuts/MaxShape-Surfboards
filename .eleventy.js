c4ce4f3ddeb964c6cb454f2a490df777e6bba53d
Julien Labatut <skoognuts@outlook.fr>
2022-02-24 16:05:58 +0100

    11TY: Change Config To Add Galery Collection.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 /* Configuration File */
 
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addCollection("galerie", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./src/content/galerie/*.md");
+  })
   
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/assets");
